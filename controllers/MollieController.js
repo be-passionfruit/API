15755d7a64f7fa29d158e8bceedad1f432759efe
thomdela2 <thomdela2@student.arteveldehs.be>
2022-07-27 11:11:08 +0200

    ✨ Update webhook

--- controllers/MollieController.js
+++ controllers/MollieController.js
@@ -63,8 +63,9 @@ class MollieController {
 
     /** 02. Setup a webhook route */
     webhook = async (req, res, next) => {
-        console.log('Event toegekomen met id: ', req.body.id);
-        res.status(200).json(req.body);
+      const id = await req.body.id;
+      console.log('Event toegekomen met id: ', id);
+      res.status(200).send(id);
     }
     
 }
